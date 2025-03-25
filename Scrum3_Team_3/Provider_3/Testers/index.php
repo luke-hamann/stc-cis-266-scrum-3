619f906ab794e9e6a2606b47401817fe38276136
Luke Hamann <135341515+luke-hamann@users.noreply.github.com>
2025-03-24 20:06:46 -0500

    Review AJAJ client

--- Scrum3_Team_3/Provider_3/Testers/index.php
+++ Scrum3_Team_3/Provider_3/Testers/index.php
@@ -74,8 +74,8 @@ try {
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Testers</title>
 </head>
 <body>
